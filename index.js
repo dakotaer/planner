e81651be62998886fe7c8809d4824cd561a13268
dakotaer <dakota.e.nyc@gmail.com>
2025-04-22 18:28:51 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 
 app.use(express.static('public'));
 
 app.get('/rules', (req, res) => {
-  const rules = require('./rules.json');
+  const rules = require('rules.json');
   res.json(rules);
 });
 
 app.get('/courses', (req, res) => {
-  const courses = require('./courses.json');
+  const courses = require('courses.json');
   res.json(courses);
-});
-
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
 });
